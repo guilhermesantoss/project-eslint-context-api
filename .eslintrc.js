657c850bfecb4ba758445fdfc27270dbf532c73b
Guilherme Santos <g.santos.sfc@gmail.com>
2021-09-22 21:46:35 -0300

    config inicial do projeto

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComa: 'all',
+  allowParens: 'avoid',
+  printWidth: 80,
+  tabWidth: 2,
+};
